f38d6d3e36ec4afcbacb097486bd70de0b675a22
aphraz <aphraz@live.com>
2020-06-12 02:23:25 +0500

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -6,6 +6,6 @@ systemctl daemon-reload
 /etc/init.d/php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm restart 
 for i in $(ls -l | grep '^d' | awk '{print $9}'); \
 do echo 'Fixing permissions for' $i;\
-chown -R $i:www-data $i/public_html;\
-find $i/public_html/ -type d -not -perm 775 -exec chmod 775 {} \;\
-find $i/public_html -type f -not -perm 664 -exec chmod 664 {} \;; done
+chown -R $i:www-data /home/master/applications/$i/public_html;\
+find /home/master/applications/$i/public_html/ -type d -not -perm 775 -exec chmod 775 {} \;\
+find /home/master/applications/$i/public_html -type f -not -perm 664 -exec chmod 664 {} \;; done
