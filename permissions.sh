f249e15a362db656a0fd2d1724a724b167f5e779
aphraz <aphraz@live.com>
2020-06-12 02:53:10 +0500

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -4,8 +4,8 @@ appsDir=/home/master/applications
 sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service
 systemctl daemon-reload
 /etc/init.d/php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm restart 
-for i in $(ls -l | grep '^d' | awk '{print $9}'); \
-do echo 'Fixing permissions for' $i; \
+source ~/.bashrc
+for i in $(ls -l $appsDir/| grep '^d' | awk '{print $9}');do echo 'Fixing permissions for' $i; \
 chown -R $i:www-data $appsDir/$i/public_html; \
 find $appsDir/$i/public_html/ \
 \( \
