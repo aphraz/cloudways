14a55424bf9ccc5b48e74f411915009cdc9f4d29
aphraz <aphraz@live.com>
2020-06-12 02:19:13 +0500

    Create permissions.sh

---
+++ permissions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FPM=php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm
+
+sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service
+systemctl daemon-reload
+/etc/init.d/php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm restart 
+for i in $(ls -l | grep '^d' | awk '{print $9}'); \
+do echo 'Fixing permissions for' $i;\
+chown -R $i:www-data $i/public_html;\
+find $i/public_html/ -type d -not -perm 775 -exec chmod 775 {} \;\
+find $i/public_html -type f -not -perm 664 -exec chmod 664 {} \;; done
