e0b1e0d0f45ab3432a23c76ef3b7258ed6e7d2a0
aphraz <aphraz@live.com>
2020-06-12 02:43:35 +0500

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 FPM=php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm
-
+appsDir=/home/master/applications
 sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service
 systemctl daemon-reload
 /etc/init.d/php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm restart 
 for i in $(ls -l | grep '^d' | awk '{print $9}'); \
-do echo 'Fixing permissions for' $i;\
-chown -R $i:www-data /home/master/applications/$i/public_html;\
-find /home/master/applications/$i/public_html/ -type d -not -perm 775 -exec chmod 775 {} \;\
-find /home/master/applications/$i/public_html -type f -not -perm 664 -exec chmod 664 {} \;; done
+do echo 'Fixing permissions for' $i; \
+chown -R $i:www-data $appsDir/$i/public_html; \
+find $appsDir/$i/public_html/ \
+\( \
+-type d -not -perm 775 -exec chmod 775 {} \; \
+\) \
+-or \
+\( \
+-type f -not -perm 664 -exec chmod 664 {} \; \
+\) \
+; done
