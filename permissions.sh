d59c26af8a4b832949f9414ba34439c2e29eee27
aphraz <aphraz@live.com>
2020-06-22 23:07:28 +0500

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 FPM=php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm
 appsDir=/home/master/applications
-sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service
-systemctl daemon-reload
-/etc/init.d/$FPM restart 
+if grep -q "UMask=0002" /lib/systemd/system/$FPM.service
+then
+    echo "The UMask is already added"
+else
+    sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service
+    systemctl daemon-reload
+    /etc/init.d/$FPM restart 
+fi
 source ~/.bashrc
 for i in $(ls -l $appsDir/| grep '^d' | awk '{print $9}');do echo 'Fixing permissions for' $i; \
 chown -R $i:www-data $appsDir/$i/public_html; \
