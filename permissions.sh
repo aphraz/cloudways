23e6943996a985d0d3939e01df58504d477ca8eb
aphraz <aphraz@live.com>
2020-06-25 00:50:21 +0500

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-FPM=php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm
+FPM=php$(php -v  | head -n 1 | cut -d " " -f2 | cut -d "." -f1,2)-fpm    # Getting PHP version
 appsDir=/home/master/applications
-if grep -q "UMask=0002" /lib/systemd/system/$FPM.service
+if grep -q "UMask=0002" /lib/systemd/system/$FPM.service                 # Making sure the line is not added already
 then
-    echo "The UMask is already added"
+    echo "The UMask is already added"                                    
 else
-    sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service
+    sed -i -e '/PIDFile/aUMask=0002' /lib/systemd/system/$FPM.service    # Setting appropriate Umask for PHP process
     systemctl daemon-reload
     /etc/init.d/$FPM restart 
 fi
-source ~/.bashrc
+
 for i in $(ls -l $appsDir/| grep '^d' | awk '{print $9}')
 do 
     echo 'Fixing permissions for' $i 
-    chown -R $i:www-data $appsDir/$i/public_html
+    chown -R $i:www-data $appsDir/$i/public_html                         # Setting ownership of files as to application users (Optional) 
     find $appsDir/$i/public_html/ \
-        -type d -print0 | xargs -0 chmod 775
+        -type d -print0 | xargs -0 chmod 775                             # Correcting permissions for already files and directories
     find $appsDir/$i/public_html/ \
         -type f -print0 | xargs -0 chmod 664
 done
