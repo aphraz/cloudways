48b7f3befad64e179372f6694d76ea3ebbd6199f
aphraz <aphraz@live.com>
2021-12-22 16:08:41 +0500

    Create wafallow.sh

---
+++ wafallow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter the app DB name: " APP
+/usr/bin/awk '/set_real_ip/{print substr($NF, 1, length($NF)-1),"1;"}' \
+	/etc/nginx/proxies/* > /etc/nginx/proxies/waf-allowedIPs
+/bin/sed -i -e '1s|^|geo $realip_remote_addr $allowed {\
+        proxy 127.0.0.1;\
+        default 0;\
+        include "\/etc\/nginx\/proxies\/waf-allowedIPs";\
+        }\
+|' -e '/location @backend /i\
+  if ($allowed = 0){\
+	return 403;\
+  }' /etc/nginx/sites-available/"${APP}"
+
+/etc/init.d/nginx restart
