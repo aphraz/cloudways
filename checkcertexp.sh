222e3376a42d412f59658ad27e642f0dbf998483
aphraz <aphraz@live.com>
2022-04-26 16:04:31 +0500

    Update checkcertexp.sh

--- checkcertexp.sh
+++ checkcertexp.sh
@@ -3,7 +3,7 @@
 for A in $(ls -l /home/master/applications/| grep "^d" | awk '{print $NF}'); 
 	do 
 		echo $A && expiry=$(sudo openssl x509 -in /home/master/applications/$A/ssl/server.crt -noout -dates | \
-			grep "notAfter" | cut -d '=' -f2)
+		grep "notAfter" | cut -d '=' -f2)
 		current=$(TZ=GMT date --date="now" '+%b %d %H:%M:%S %Y GMT')
 		norm_expiry=$(date -d"$expiry" +%s)
 		norm_current=$(date -d"$current" +%s)
