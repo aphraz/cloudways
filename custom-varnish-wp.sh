875d11703ac9437e5b7c2e772b46f47d762e41e3
aphraz <aphraz@live.com>
2022-06-27 19:24:10 +0500

    Update custom-varnish-wp.sh

--- custom-varnish-wp.sh
+++ custom-varnish-wp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 APP_DIR="/home/$HOSTNAME"
-for app in $(ls -l $APP_DIR/ | awk '/^d/ {print $NF}');
+for app in $(ls -l /home/master/applications/| awk '/^d/ {print $NF}');
 	do
 		app_type=$(awk '/server_name/ {split($2,a,"-") ; print a[1]; exit}' $APP_DIR/$app/conf/server.nginx)
-		if [[ $app_type == "^(wordpress|woocommerce" ]]; then
-			cat <<- _EOF_ >> "$APP_DIR/$app/conf/custom_recv.vcl"
+		if [[ $app_type =~ ^wo ]]; then
+			echo "App $app is $app_type. Adding custom VCL."
+			cat <<- _EOF_ >> $APP_DIR/$app/conf/custom-recv.vcl
 			if (req.url ~ "/(contact)") {
 			return (pipe);
 			}
