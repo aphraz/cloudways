8137f30b8c2c9f3894886f4b9ea55fbc36356721
Afraz Ahmed <aphraz@live.com>
2023-03-05 11:46:51 +0500

    Added spacing

--- varnish-mobile.sh
+++ varnish-mobile.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
+echo " "
 echo -e "Downloading the device detection VCL"
+echo " "
+
 /usr/bin/curl -s https://raw.githubusercontent.com/aphraz/cloudways/master/devicedetect.vcl > /etc/varnish/devicedetect.vcl
 
 echo -e "Adding necessary includes in and directives (vcl_recv,vcl_hash) /etc/varnish/cloudways.vcl for WordPress"
-
+echo " "
 sed -i -e '/\/etc\/varnish\/vars.vcl/ainclude "/etc/varnish/devicedetect.vcl";' \
 -e '/sub vcl_recv/acall devicedetect;' \
 -e '/\/etc\/varnish\/https.vcl/i\\tif(req.http.X-UA-Device ~ "^(mobile|tablet)\-.+$") {\n\t\tset req.http.X-UA-Device = "mobile";\n\t} else {\n\t\tset req.http.X-UA-Device = "desktop";\n\t}' \
 -e '/\/etc\/varnish\/hash\/geoip.vcl/i\\tif (!(req.url ~ ".(gif|jpg|jpeg|swf|flv|mp3|mp4|pdf|ico|png|gz|tgz|bz2)?($|\\?)")) {\n\t\thash_data(req.http.X-UA-Device);\n\t}' /etc/varnish/cloudways.vcl
 
+echo " "
 echo -e "Adding necessary directives for Magento 2 (/etc/varnish/hash/magento2.vcl)"
+echo " "
 
 cat << _EOF_ >> /etc/varnish/hash/magento2.vcl
 	if (!(req.url ~ ".(gif|jpg|jpeg|swf|flv|mp3|mp4|pdf|ico|png|gz|tgz|bz2)?($|\\?)")) {
@@ -19,6 +23,9 @@ cat << _EOF_ >> /etc/varnish/hash/magento2.vcl
 _EOF_
 
 echo -e "Restarting Varnish"
+
+echo " "
 echo -e "Please verify if everything is working correctly."
+echo " "
 /etc/init.d/varnish restart
 
