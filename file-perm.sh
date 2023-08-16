23bcd84e9a447257d4f2dd5e329c791b43fb3f0d
Afraz Ahmed <aphraz@live.com>
2023-08-16 22:21:39 +0500

    corrected file-perm.sh

--- file-perm.sh
+++ file-perm.sh
@@ -6,11 +6,11 @@ for app in $(ls -l $APP_DIR/| awk '/^d/ {print $NF}');
 		app_type=$(awk '/server_name/ {split($2,a,"-") ; print a[1]; exit}' $APP_DIR/$app/conf/server.nginx)
 		if [[ ${app_type} =~ ^wo ]] ; then
 			if [[ -f $APP_DIR/$app/private_html/script.sh ]]; then
-				echo 'App: $app is WordPress and the script.sh is there. Setting Ownership.'
+				echo "App: $app is WordPress and the script.sh is there. Setting Ownership."
 				sudo chown $app:www-data $APP_DIR/$app/private_html/script.sh
 				sudo chmod 775 $APP_DIR/$app/private_html/script.sh
                 	else
-       				echo 'App: $app is WordPress but script.sh is not there. Creating and Setting Ownership.'
+       				echo "App: $app is WordPress but script.sh is not there. Creating and Setting Ownership."
 				touch $APP_DIR/$app/private_html/script.sh
 				sudo chown $app:www-data $APP_DIR/$app/private_html/script.sh
 				sudo chmod 775 $APP_DIR/$app/private_html/script.sh
