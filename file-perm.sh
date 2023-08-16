877fdec9442f651fed32655e8fc3b0fa128870db
Afraz Ahmed <aphraz@live.com>
2023-08-16 22:27:29 +0500

    corrected file-perm.sh

--- file-perm.sh
+++ file-perm.sh
@@ -5,15 +5,15 @@ for app in $(ls -l $APP_DIR/| awk '/^d/ {print $NF}');
 	do
 		app_type=$(awk '/server_name/ {split($2,a,"-") ; print a[1]; exit}' $APP_DIR/$app/conf/server.nginx)
 		if [[ ${app_type} =~ ^wo ]] ; then
-			if [[ -f $APP_DIR/$app/private_html/script.sh ]]; then
-				echo "App: $app is WordPress and the script.sh is there. Setting Ownership."
-				sudo chown $app:www-data $APP_DIR/$app/private_html/script.sh
-				sudo chmod 775 $APP_DIR/$app/private_html/script.sh
+			if [[ -f $APP_DIR/$app/private_html/services.sh ]]; then
+				echo "App: $app is WordPress and the services.sh is there. Setting Ownership."
+				sudo chown $app:www-data $APP_DIR/$app/private_html/services.sh
+				sudo chmod 775 $APP_DIR/$app/private_html/services.sh
                 	else
-       				echo "App: $app is WordPress but script.sh is not there. Creating and Setting Ownership."
-				sudo touch $APP_DIR/$app/private_html/script.sh
-				sudo chown $app:www-data $APP_DIR/$app/private_html/script.sh
-				sudo chmod 775 $APP_DIR/$app/private_html/script.sh
+       				echo "App: $app is WordPress but services.sh is not there. Creating and Setting Ownership."
+				sudo touch $APP_DIR/$app/private_html/services.sh
+				sudo chown $app:www-data $APP_DIR/$app/private_html/services.sh
+				sudo chmod 775 $APP_DIR/$app/private_html/services.sh
 			fi
 		else
 			echo "App: $app is not WordPress. Skipping.."
